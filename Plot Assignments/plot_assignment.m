2d72b9a882922942087dde35f12f4e47f0427106
Alexander Edberg <alexander_edberg@hotmail.com>
2023-08-26 15:58:56 +0200

    finnished the plot assignment

--- Plot Assignments/plot_assignment.m
+++ Plot Assignments/plot_assignment.m
@@ -7,7 +7,7 @@ close all
 load datatraffic.mat
 
 %2
-plot(years,traffic)
+plot(years,traffic) %plot Y versus the corresponding values in X
 
 title('Monthly amounts of data traffic each year')
 xlabel('Year')
@@ -16,7 +16,7 @@ legend('Mobile Data (2G/3G/4G)', 'Mobile Data (5G)', 'Fixed Wireless Access (3G/
 
 
 %3
-bar(years,traffic)
+bar(years,traffic) %bar chart plot
 
 title('Monthly amounts of data traffic each year')
 xlabel('Year')
@@ -35,7 +35,7 @@ legend('Mobile Data (2G/3G/4G)', 'Mobile Data (5G)', 'Fixed Wireless Access (3G/
 
 
 subplot(2,1,2); % (n,m,p), n=rows , m=columns, p =position for plot
-bar(years,traffic,'stacked')
+bar(years,traffic,'stacked') %stacked to get it stacked
 
 title('Monthly amounts of data traffic each year')
 xlabel('Year')
@@ -50,10 +50,42 @@ traffic_avg_per = traffic_avg_per*10^9; %Go from exabyte (10^18 byte) to gigabyt
 
 plot(years,traffic_avg_per)
 
-title('Monthly amounts of data traffic each year')
+title('Monthly amounts of data traffic each year for the average user')
 xlabel('Year')
 ylabel('Gigabyte (10^{9} bytes)')
 legend('Mobile Data (2G/3G/4G)', 'Mobile Data (5G)', 'Fixed Wireless Access (3G/4G/5G)', 'Location', 'northwest')
+
+
+%6-7
+
+plot(years,sum(traffic_avg_per,2),'k-', years,traffic_avg_per(:,1),'b-.', years,traffic_avg_per(:,3), 'm:', years,traffic_avg_per(:,2),'r--')%,LineWidth=0.5)
+
+title('Monthly amounts of data traffic each year for the average user')
+xlabel('Year')
+ylabel('Gigabyte (10^{9} bytes)')
+
+legend('Total data traffic', 'Mobile Data (2G/3G/4G)', 'Fixed Wireless Access (3G/4G/5G)', 'Mobile Data (5G)', 'Location', 'northwest')
+
+%8
+
+saveas(gcf,'Data_traffic.fig')
+saveas(gcf,'Data_traffic.jpg') %unsharp, bad for reports due to the unsharpness
+saveas(gcf,'Data_traffic.png') %unsharp, bad for reports due to the unsharpness
+saveas(gcf,'Data_traffic.pdf') %sharp, bad for reports the size of the "figure" is a whole a4 with a lot of blank space padding
+saveas(gcf,'Data_traffic.eps', 'epsc') %added epsc flag here to get color, sharp and good for reports
+
+%   Yes the resized changes carried over.
+
+%9
+
+%    Looks fine
+
+
+
+
+
+
+
 
 
 
